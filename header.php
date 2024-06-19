8517366119e6da7d0c88c28b5ef162e838b0b39f
rod773 <rodrigoarcsdio1@gmail.com>
2024-06-19 10:35:54 +0200

    update

--- header.php
+++ header.php
@@ -5,7 +5,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo get_bloginfo('name');?></title>
+
+    <?php $title = get_bloginfo( 'name' );?>
+
+    <title><?php echo $title?></title>
 
     <?php  wp_head(); ?>
 </head>
